93185d848d9b1409902bddf544671038f57a0f37
BlessedRebuS <patrick.difazio@studio.unibo.it>
2022-06-08 19:30:38 +0200

    aggiunto copy.sh

--- Blob2/usefulScripts/copy.sh
+++ Blob2/usefulScripts/copy.sh
@@ -1,18 +1,20 @@
 #!/bin/bash
 
 #copio la configurazione ssh di vagrant dentro quella della macchina host
-vagrant ssh-config >> ~/.ssh/config
+vagrant ssh-config > ~/.ssh/config
 
 DIR="consegna"
 PATH_VM="/home/vagrant"
-declare -a arr=("client1" "client2" "router")
+INTEFACES="/etc/network/interfaces"
+INTEFACESD="/etc/network/interfaces.d/*"
+
+
+declare -a arr=("client" "server" "router1" "router2")
 
 for i in "${arr[@]}"
 do
    echo "Consegno: $i"
    mkdir -p $DIR/$i
-   scp -rp vagrant@$i:"$PATH_VM/* $PATH_VM/.bash_history" $DIR/$i   
-   
+   scp -rp vagrant@$i:"$PATH_VM/* $PATH_VM/.bash_history $INTEFACES $INTEFACESD" $DIR/$i   
 done
-
 tar czf ammsis.tar.gz $DIR
